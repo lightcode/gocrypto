a1c7e93573771b43928f50885fd8c8b2e9f2c4ac
lightcode <lightcode@gmx.com>
2015-12-15 14:51:14 +0100

    Teste le chiffrement d'un grand fichier avec Elgamal

--- elgamal_test.go
+++ elgamal_test.go
@@ -9,7 +9,7 @@ func TestElgamalEncryption(t *testing.T) {
 	keys := GenerateElgamalKeys(160)
 
 	m := make([][]byte, 2)
-	m[0] = randomBytes(180 / 8)
+	m[0] = randomBytes(10000 / 8)
 	m[1] = []byte{0, 0, 54, 89, 75, 31, 0, 0, 0}
 
 	for _, m1 := range m {
